0d2ca9bf4af147f832ba83d7793f80f0bfa2ce80
EduRamos95 <eduramos08.30@gmail.com>
2022-06-07 10:11:19 -0700

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * puts - Writes the string s and a trailing newline to stdout
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

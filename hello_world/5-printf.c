2ae9a8caa80f0fbdab0a7b0a9d75190bddd50a74
EduRamos95 <eduramos08.30@gmail.com>
2022-06-07 10:22:20 -0700

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * printf - Write the output under the control of a 
+  * format string that specifies how subsequent 
+  * arguments are converted for output. don't end with newline.
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

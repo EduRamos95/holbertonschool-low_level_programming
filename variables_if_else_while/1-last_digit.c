1941af9047f686aef644a3a1acabace255a909e0
EduRamos95 <eduramos08.30@gmail.com>
2022-06-07 18:30:41 -0700

    last digit of ramdon number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Randow number and evaluate the last digit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n,last_dig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_dig = n%10;
+	/* your code goes there */
+	if (last_dig > 5)
+		printf("the last digit of %d is %d and is greater than 5\n", n, last_dig);
+	else if (last_dig == 0)
+		printf("the last digit of %d is %d and is 0\n", n, last_dig);
+	else
+		printf("the last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
+	return (0);
+}

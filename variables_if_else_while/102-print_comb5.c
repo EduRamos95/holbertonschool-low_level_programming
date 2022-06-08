738279bcc03538974fe09ad5739b84dc3100b135
EduRamos95 <eduramos08.30@gmail.com>
2022-06-07 22:38:33 -0700

    Combinate 2 number with 2 digits

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: Combination 00-99, print 2 digit per number
+  * Don't use type 'char', Only use putchar 8 times
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int dec1, unid1, dec2, unid2;
+
+	/*Your code goes there*/
+	for (dec1 = '0'; dec1 <= '9'; dec1++)
+	{
+		for (unid1 = '0'; unid1 <= '9'; unid1++)
+		{
+			for (dec2 = dec1; dec2 <= '9'; dec2++)
+			{
+				for (unid2 = (unid1 + 1); unid2 <= '9'; unid2++)
+				{
+					putchar(dec1);
+					putchar(unid1);
+					putchar(' ');
+					putchar(dec2);
+					putchar(unid2);
+
+					if (!((dec1 == '9' && unid1 == '8') && (dec2 == '9' && unid2 == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				unid2 = 0;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

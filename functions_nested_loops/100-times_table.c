7d950c7cf8ec221254296bc14ff66938b6d5d510
EduRamos95 <eduramos08.30@gmail.com>
2022-06-09 10:53:02 -0700

    Multiplicacion table of 'n'

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,51 +10,37 @@ void print_times_table(int n)
 	int ROW;
 	int COL;
 	int MUL;
-	int M1u; /*dig unid of product*/
-	int M2d; /*dig dec of product*/
-	int M3c; /*dig cent of product*/
 
 	if (n >= 0 && n <= 15)
 	{
-		for (ROW = 0; ROW <= n; ROW++)
+		for (ROW = 0; n >= ROW; ROW++)
 		{
-			for (COL = 0; COL <= n; COL++)
+			_putchar('0');
+			for (COL = 1; n >= COL; COL++)
 			{
 				MUL = ROW * COL;
-				if (COL == 0)
+				_putchar(',');
+				_putchar(' ');
+				if (MUL <= 9)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar('0' + MUL);
 				}
-				else
+				else if (MUL >=  10 && MUL <= 99)
 				{
-					_putchar(',');
 					_putchar(' ');
-					if (MUL <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + MUL);
-					}
-					else if (MUL >=  10 && MUL <= 99)
-					{
-						M1u = MUL % 10;
-						M2d = MUL / 10;
-						_putchar(' ');
-						_putchar('0' + M2d);
-						_putchar('0' + M1u);
-					}
-					else if (MUL >= 100 && MUL < 999)
-					{
-						M1u = MUL % 10;
-						M2d = (MUL / 10) % 10;
-						M3c = MUL / 100;
-						_putchar('0' + M3c);
-						_putchar('0' + M2d);
-						_putchar('0' + M1u);
-					}
+					_putchar('0' + (MUL / 10));
+					_putchar('0' + (MUL % 10));
+				}
+				else if (MUL >= 100 && MUL < 999)
+				{
+					_putchar('0' + (MUL / 100));
+					_putchar('0' + ((MUL / 10) % 10));
+					_putchar('0' + (MUL % 10));
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }

e403122027aa51f96bab10904d1e0c11b50901d8
EduRamos95 <eduramos08.30@gmail.com>
2022-06-15 20:37:44 -0700

    updates the value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: pointer int 'n'
+ */
+void reset_to_98(int *n)
+{
+	/*put '98' in the address of variable 'n'*/
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,7 +4,14 @@
 /**
  * _putchar - puts character to standart output
  * @c: character to put to standart output
+ * Return: 1 is success
  */
 int _putchar(char c);
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: pointer int 'n'
+ */
+void reset_to_98(int *n);
 
 #endif

eba540f283396318303bec28dca4a8a9db153d73
EduRamos95 <eduramos08.30@gmail.com>
2022-06-16 15:22:40 -0700

    copies the string pointed to

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int lenght;
+
+        lenght = 0;
+	/*'\0' it's refer to endline*/
+        while (*s != '\0')
+        {
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest.
+ * @dest: pointer
+ * @src: pointer
+ * Return: copy of original source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -64,4 +64,13 @@ void puts_half(char *str);
  * @n: number of elements
  */
 void print_array(int *a, int n);
+
+/**
+ * *_strcpy - copies the string pointed to by src, including the terminating null byte (\0)
+ * to the buffer pointed to by dest.
+ * @dest: pointer
+ * @src: pointer
+ */
+char *_strcpy(char *dest, char *src);
+
 #endif

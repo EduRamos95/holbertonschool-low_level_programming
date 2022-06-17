59fd570a76269f183a0346b5dac267f92f7335b7
EduRamos95 <eduramos08.30@gmail.com>
2022-06-17 14:30:08 -0700

    fills memory with a constant byte

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -137,4 +137,14 @@ char *cap_string(char *s);
  * Return: string
  */
 char *leet(char *s);
+
+
+/**
+ * _memset - fill n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: memory area
+ */
+char *_memset(char *s, char b, unsigned int n);
 #endif

c8b7d792090671531d33d8a3aa2e14af145a7c37
EduRamos95 <eduramos08.30@gmail.com>
2022-06-20 19:01:44 -0700

    prints a string,and newline

---
+++ recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s);
+#endif

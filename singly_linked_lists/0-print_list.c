09b145578f74f7365fcaccfaaf77bf0249e56e41
EduRamos95 <eduramos08.30@gmail.com>
2022-07-13 23:05:30 -0500

    prints elements of a linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: linked list
+ * Return: number of nodes in linked list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int number_of_elements = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		number_of_elements++;
+		h = h->next;
+	}
+	return (number_of_elements);
+}

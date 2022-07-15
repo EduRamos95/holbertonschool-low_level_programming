d2cdda304d5072ffd9b17eea3aa660c72e487c44
EduRamos95 <eduramos08.30@gmail.com>
2022-07-14 20:06:55 -0500

    frees a linked list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -4,11 +4,11 @@
  *
  * @head: A pointer to the first node of the list to free
  */
-void _free_list(list_t *head)
+void free_list(list_t *head)
 {
 	if (head)
 	{
-		_free_list(head->next);
+		free_list(head->next);
 		if (head->str)
 			free(head->str);
 		free(head);

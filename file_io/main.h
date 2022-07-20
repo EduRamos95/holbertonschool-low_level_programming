b9cddaed81e9d2bd663c56465f646b0a0d31e1d6
EduRamos95 <eduramos08.30@gmail.com>
2022-07-19 22:33:09 -0500

    start

---
+++ file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* close */
+#include <unistd.h>
+
+/* dprintf */
+#include <stdio.h>
+
+/* malloc, free */
+#include <stdlib.h>
+
+int _putchar(char c);
+#endif
